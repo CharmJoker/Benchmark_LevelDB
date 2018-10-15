b98d88478cc57d05cc2b956b8d28f9d7e8c37117
Jiapeng Chi <jiapengchi@gmail.com>
2018-10-15 19:28:50 -0400

    Code

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # LevelDB_Benchmark
+
+Plyvel:
+1000 records:
+0.08093023300170898 seconds
+
+10000 records:
+0.16175317764282227 seconds
+
+100000 records:
+Total time: 1.4124243259429932 seconds
--- plyvel_performance.py
+++ plyvel_performance.py
@@ -4,31 +4,48 @@ import random
 import string
 from functools import wraps
 
+
 def fn_timer(function):
     @wraps(function)
     def function_timer(*args, **kwargs):
         t0 = time.time()
         result = function(*args, **kwargs)
         t1 = time.time()
-        print ("Total time running %s: %s seconds" %
-                (function.func_name, str(t1-t0))
-                )
+        print("Total time: %s seconds" %
+              (str(t1 - t0))
+              )
         return result
+
     return function_timer
+
 
 def initialize():
     db = plyvel.DB('/home/jiapeng/leveldb')
     return db
 
+
 def id_generator(size=8, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
+
 
 def insert(db, key, value):
     db.put(key, value)
 
+
+@fn_timer
+def batch_insert(db, count):
+    for num in range(0, count):
+        insert(db, bytes(id_generator(), encoding="utf8"), bytes(id_generator(), encoding="utf8"))
+
+
 # Experiment 1 – performance to write a key-value pair
 
 # The calculated time to write key-value pairs (ms).
+db = initialize()
+batch_insert(db,100000)
+db.close()
+db.closed
+
 # Memory usages for write operation (MB).
 #
 # Experiment 2 – performance to read value corresponding to a given key
